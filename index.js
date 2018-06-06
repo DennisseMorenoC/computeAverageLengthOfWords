529f6ab43922b7e5c3c7b4c7f9a86047c9e8e059
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-06-06 09:17:50 -0400

    EjercicioRetosdelCodigo-LengthWords

---
+++ index.js
@@ -0,0 +1,7 @@
+function computeAverageLengthOfWords(word1, word2) {
+    let promedio = (word1.length + word2.length) / 2;
+    return promedio;
+}
+console.log("Promedio > " + computeAverageLengthOfWords("dennisse", "moreno"));  
+ 
+  
